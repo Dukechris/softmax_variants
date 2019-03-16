53efac6f43e262e19d4488f111db1af081554622
Dukechris <zhangxiao1688@hotmail.com>
2019-03-17 01:28:54 +0800

    back

--- train_mnist_center_loss.py
+++ train_mnist_center_loss.py
@@ -32,7 +32,7 @@ def visualize(feat, weights, labels, epoch):
     plt.ion()
     c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff',
          '#ff00ff', '#990000', '#999900', '#009900', '#009999']
-    # plt.clf()
+    plt.clf()
     for i in range(10):
         plt.plot(feat[labels == i, 0], feat[labels == i, 1], '.', c=c[i], markersize=0.3)
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
